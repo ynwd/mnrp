1b9b531072cfb1d1711d6c1976c7e945a71ecaad
ynwdd <10122431+ynwdd@users.noreply.github.com>
2021-11-01 22:31:14 +0700

    update

--- web/modules/home/src/App.tsx
+++ web/modules/home/src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 async function getList() {
-  return await fetch('http://localhost:8080/api')
+  return await fetch('/api')
 }
 
 function App() {

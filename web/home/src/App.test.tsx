1153d9bb24b7e84f129f50240aaf994c981f26ea
ynwdd <10122431+ynwdd@users.noreply.github.com>
2021-11-04 22:08:50 +0700

    rm modules

--- web/home/src/App.test.tsx
+++ web/home/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const pElement = screen.getByText(/The Best Interface Is No Interface/i);
-  expect(pElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
